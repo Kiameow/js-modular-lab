fcffc1ae5a18a155439bd4141eff4c883dd09827
Kiameow <zjzootl@qq.com>
2024-01-19 14:19:55 +0800

    esm init

---
+++ src/robot.js
@@ -0,0 +1,25 @@
+import { findRoute } from "./route.js";
+import { roadGraph } from "./road.js";
+
+function randomPick(array) {
+  let choice = Math.floor(Math.random() * array.length);
+  return array[choice];
+}
+
+function randomRobot(state) {
+  return { direction: randomPick(roadGraph[state.place]) };
+}
+
+function goalOrientedRobot({ place, parcels }, route) {
+  if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+      route = findRoute(roadGraph, place, parcel.place);
+    } else {
+      route = findRoute(roadGraph, place, parcel.address);
+    }
+  }
+  return { direction: route[0], memory: route.slice(1) };
+}
+
+export {randomRobot, goalOrientedRobot};
