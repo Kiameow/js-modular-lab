f27355aa260cc94f9d41e1ca33a293a02125ec61
Kiameow <zjzootl@qq.com>
2024-01-28 15:54:42 +0800

    babel-version finished

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,23 @@ const path = require("path");
 
 module.exports = {
   entry: "./index.js",
+  target: ['web', 'es5'],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
   resolve: {
     extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+	test: /\.js$/,
+	exclude: /node_modules/,
+	use: {
+	  loader: 'babel-loader'
+	}
+      }
+    ]
   },
 };
